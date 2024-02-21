42db4f91aaffe16bc78e1b87c2cd81715368a969
Noah Call <a02361280@aggies.usu.edu>
2024-02-20 19:21:44 -0700

    Added my own file, added weakly dominated strategy

--- Program3/Normal_Form.py
+++ Program3/Normal_Form.py
@@ -62,6 +62,40 @@ def identify_strictly_dominated_strategies(matrix):
 
     return row_dominated, col_dominated
 
+def identify_weakly_dominated_strategies(matrix):
+    num_rows = len(matrix[0])
+    num_cols = len(matrix[0][0])
+
+    # Get the header titles
+    col_headers = [chr(ord('Z') - i) for i in range(num_cols)]
+    col_headers.reverse()
+
+    row_letters = [chr(ord('A') + i) for i in range(num_rows)]
+
+    row_dominated = [False] * num_rows
+    col_dominated = [False] * num_cols
+
+    # Check for weakly row dominance
+    for i in range(num_rows):
+        for j in range(num_rows):
+            if i != j and not row_dominated[i]:
+                row_i_weak_dom = all(matrix[0][i][k] <= matrix[0][j][k] for k in range(num_cols))
+                if row_i_weak_dom:
+                    row_dominated[i] = True
+
+    # Check for weakly column dominance
+    for k in range(num_cols):
+        for l in range(num_cols):
+            if k != l and not col_dominated[k]:
+                col_k_weak_dom = all(matrix[1][i][k] <= matrix[1][i][l] for i in range(num_rows))
+                if col_k_weak_dom:
+                    col_dominated[k] = True
+
+    print("Row player weakly dominated strategies:", [row_letters[i] for i, dominated in enumerate(row_dominated) if dominated])
+    print("Column player weakly dominated strategies:", [col_headers[k] for k, dominated in enumerate(col_dominated) if dominated])
+
+    return row_dominated, col_dominated
+
 def print_normal_form_table(matrix):
     print(matrix)
     num_rows = len(matrix[0])
@@ -94,7 +128,7 @@ def print_normal_form_table(matrix):
         print(row_letters[i], end = " ")
         for j in range(num_cols):
             print(" | ", end = " ")
-            for k in range(2):
+            for k in range(num_rows):
                 print(matrix[k][i][j], " ", end = " ")
         print("|")
     
@@ -102,11 +136,13 @@ def print_normal_form_table(matrix):
 file_pathA = "Program3/data/prog3A.txt"
 file_pathB = "Program3/data/prog3B.txt"
 file_pathC = "Program3/data/prog3C.txt"
+my_file = "Program3/data/myProg3File.txt"
 
-files = [file_pathA, file_pathB, file_pathC]
+files = [file_pathA, file_pathB, file_pathC, my_file]
 
 for path in files:
     matrix = read_payoff_matrix(path)
-    print("\n", path.strip("Program3/data/"))
+    print("\n", path.strip("Program3/data"))
     print_normal_form_table(matrix)
     identify_strictly_dominated_strategies(matrix)
+    identify_weakly_dominated_strategies(matrix)
